b48a920ea056005cf16db39b9e3db08510a54b63
A. K. A. Kurniawan <mail.ansel@gmail.com>
2019-06-14 16:41:37 +0700

    Update storybook for widget version 1.0.2

---
+++ storybooks/widget/main.a47ea9f75386970ad86c.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(410),p(486),n.exports=p(1101)},486:function(n,o,p){"use strict";p.r(o);p(487),p(684),p(888),p(900),p(1092),p(1097)}},[[0,1,2]]]);
