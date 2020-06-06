44a7bcf174ef779180f41a389f502248f39b9dcb
A. K. A. Kurniawan <mail.ansel@gmail.com>
2020-06-07 01:38:11 +0700

    Update widget storybook

---
+++ storybooks/widget/main.2d7a2c32ac557b0e6e0a.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1845:function(n,o,c){"use strict";c.r(o);c(1846),c(1883),c(2069),c(2228),c(2233),c(2238)},700:function(n,o,c){c(701),c(1023),n.exports=c(1845)},812:function(n,o){}},[[700,1,2]]]);
